fed1e06e1f9e7daba6c68edb1988689b3617e185
Scott Francis <scott.francis@shopify.com>
2013-09-19 18:42:15 -0400

    WIP

---
+++ files/brews/lua_cityhash.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class LuaCityhash < Formula
+  homepage 'https://github.com/csfrancis/lua-cityhash'
+  head 'https://github.com/csfrancis/lua-cityhash.git', :using => :git
+  version '0.0.1-boxen'
+
+  depends_on 'lua'
+
+  def install
+    
+  end
+end
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class lua_cityhash {
+  include homebrew
+
+  homebrew::formula { 'lua_cityhash': 
+  	before => Package['boxen/brews/lua_cityhash'],
+  }
+
+  package { 'boxen/brews/lua_cityhash' :
+	ensure => '0.0.1-boxen'
+  }
 }
